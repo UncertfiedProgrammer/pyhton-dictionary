c8c62ed86ffc774ff975074d9d90cb7b90d78bea
UncertfiedProgrammer <rehanalman.2304@gmail.com>
2026-01-08 20:19:54 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+meme_dict = {
+            "CRINGE": "Something that's weird and embaraassing",
+            "LOL": "it stands for 'Laughing out loud' ",
+            "LMAO": "stands for 'laughing my ass off' "
+            }
+
+word = input("Type an internet slang you don't understand: ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("this word doesn't exist in the dictionary")
